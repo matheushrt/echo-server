28310dbbe6bffb4f6336729e04e0154f43d52d86
Matheus Souza <matheushrt@gmail.com>
2020-02-13 20:13:48 -0300

    created knexfile

---
+++ src/database/knexfile.ts
@@ -0,0 +1,43 @@
+export const development = {
+  migrations: {
+    directory: './migrations',
+    extension: 'ts'
+  },
+  client: 'postgresql',
+  connection: {
+    database: process.env.DATABASE_NAME,
+    host: '127.0.0.1'
+  }
+};
+
+export const staging = {
+  client: 'postgresql',
+  connection: {
+    database: 'my_db',
+    user: 'username',
+    password: 'password'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
+
+export const production = {
+  client: 'postgresql',
+  connection: {
+    database: 'my_db',
+    user: 'username',
+    password: 'password'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
